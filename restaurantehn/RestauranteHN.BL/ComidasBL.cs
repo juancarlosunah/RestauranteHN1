cfff77917e38644eca35f372dc7d3ba41780a790
Juan <jc.carranza@unah.hn>
2022-06-17 17:53:54 -0600

    guardando cambios

--- restaurantehn/RestauranteHN.BL/ComidasBL.cs
+++ restaurantehn/RestauranteHN.BL/ComidasBL.cs
@@ -8,41 +8,17 @@ namespace RestauranteHN.BL
 {
     public class ComidasBL
     {
+        Contexto _contexto;
+
+        public ComidasBL()
+        {
+            _contexto = new Contexto();
+        }
+
         public List<Comida> ObtenerComidas()
         {
-            var comidas1 = new Comida();
-            comidas1.id = 1;
-            comidas1.Descripcion = "Sopa de Mondongo";
-            comidas1.Precio = 180;
-
-            var comidas2 = new Comida();
-            comidas2.id = 2;
-            comidas2.Descripcion = "Baleadas";
-            comidas2.Precio = 15;
-            
-            var comidas3 = new Comida();
-            comidas3.id = 3;
-            comidas3.Descripcion = "Pollo Frito";
-            comidas3.Precio = 60;
-
-            var comidas4 = new Comida();
-            comidas4.id = 4;
-            comidas4.Descripcion = "Arroz con Pollo";
-            comidas4.Precio = 45;
-
-            var comidas5 = new Comida();
-            comidas5.id = 5;
-            comidas5.Descripcion = "Carne Asada";
-            comidas5.Precio = 120;
-
-            var listadeComidas = new List<Comida>();
-            listadeComidas.Add(comidas1);
-            listadeComidas.Add(comidas2);
-            listadeComidas.Add(comidas3);
-            listadeComidas.Add(comidas4);
-            listadeComidas.Add(comidas5);
-
-            return listadeComidas;
+           
+            return _contexto.Comidas.ToList();
         }
     }
 }
---
+++ restaurantehn/RestauranteHN.BL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestauranteHN.BL
+{
+   public  class Contexto: DbContext
+    {
+        public Contexto(): base("RestauranteHNHNDB")
+        {
+
+        }
+
+        public DbSet<Comida> Comidas { get; set; }
+     
+     }
+}
